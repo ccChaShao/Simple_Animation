85be26a6546009b55a77cface0f5d4012bc340ba
萝卜卜皮 <57029955+ccChaShao@users.noreply.github.com>
2025-10-29 23:18:05 +0800

    [DIFF] Animancer 测试代码

--- Assets/Scritps/Animancer/AnimancerTest.cs
+++ Assets/Scritps/Animancer/AnimancerTest.cs
@@ -6,11 +6,9 @@ using Animancer;
 using Sirenix.OdinInspector;
 using UnityEngine.Serialization;
 
+[RequireComponent(typeof(AnimancerComponent))]
 public class AnimancerTest : MonoBehaviour
 {
-    public AnimancerComponent animancerComponent;
-    public AnimationClip idleClip;
-    public AnimationClip runeClip;
 
     private void Awake()
     {
@@ -18,14 +16,84 @@ public class AnimancerTest : MonoBehaviour
     }
 
     [Button(ButtonSizes.Large)]
+    public void StopAnimation()
+    {
+        animancerComponent.Stop();
+    }
+
+    #region AnimationClip
+    
+    [BoxGroup("传统 - AnimationClip")] public AnimationClip idleClip;
+    [BoxGroup("传统 - AnimationClip")] public AnimationClip runClip;
+
+    [BoxGroup("传统 - AnimationClip"), Button(ButtonSizes.Medium)]
     public void PlayIdleClip()
     {
         animancerComponent.Play(idleClip);
     }
 
-    [Button(ButtonSizes.Large)]
-    public void PlayRuneClip()
+    [BoxGroup("传统 - AnimationClip"), Button(ButtonSizes.Medium)]
+    public void PlayRunClip()
     {
-        animancerComponent.Play(runeClip);
+        animancerComponent.Play(runClip);
     }
+    #endregion
+
+    #region ClipTransition
+    
+    [BoxGroup("新动画 - ClipTransition"), ReadOnly]
+    public AnimancerComponent animancerComponent;
+    
+
+    [BoxGroup("新动画 - ClipTransition/idle")] 
+    public ClipTransition idleTransition;
+
+    [BoxGroup("新动画 - ClipTransition/idle"), Button(ButtonSizes.Medium)]
+    public void PlayIdleTransition(float fadeDuration)
+    {
+       var state = animancerComponent.Play(idleTransition, fadeDuration, FadeMode.FromStart);
+    }
+    
+
+    [BoxGroup("新动画 - ClipTransition/run")] 
+    public ClipTransition runTransition;
+    
+    [BoxGroup("新动画 - ClipTransition/run"), Button(ButtonSizes.Medium)]
+    public void PlayRunTransition(float fadeDuration)
+    {
+        var state = animancerComponent.Play(runTransition, fadeDuration, FadeMode.FromStart);
+    }
+    
+    
+    [BoxGroup("新动画 - ClipTransition/mixer"), ProgressBar(0.0f, 1.0f), OnValueChanged("OnClipTransitionWeightValueChanged")]
+    public float clipTransitionWeight = 0f;
+    
+    [BoxGroup("新动画 - ClipTransition/mixer")] 
+    public LinearMixerTransition mixerTransition;
+    
+    private AnimancerState mixerState;
+
+    [BoxGroup("新动画 - ClipTransition/mixer"), Button(ButtonSizes.Medium)]
+    public void PlayMixerTransition(float fadeDuration)
+    {
+        var state = animancerComponent.Play(mixerTransition, fadeDuration, FadeMode.FromStart);
+        mixerState = state;
+    }
+
+    public void OnClipTransitionWeightValueChanged()
+    {
+        Debug.Log("charsiew : [OnClipTransitionWeightValueChanged] : " + clipTransitionWeight);
+    }
+
+    public void AnimancerTestEvent01()
+    {
+        Debug.Log("charsiew : [AnimancerTestEvent01] : " + animancerComponent.Events.Count);
+    }
+
+    public void AnimancerTestEvent02()
+    {
+        Debug.Log("charsiew : [AnimancerTestEvent02] : " + animancerComponent.Events.Count);
+    }
+
+    #endregion
 }
