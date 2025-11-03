030b24ceffb996e01759c91187e47695f126ee63
萝卜卜皮 <57029955+ccChaShao@users.noreply.github.com>
2025-11-03 17:12:28 +0800

    [DIFF] 跳跃处理

--- Assets/Scritps/Animation3th/PlayerContorller.cs
+++ Assets/Scritps/Animation3th/PlayerContorller.cs
@@ -12,16 +12,25 @@ namespace Animation3th
         private PlayerInputController m_InputController;
         private Animator m_Animator;
         
-        [Header("输入")]
-        private Vector3 m_Direction;
+        [Header("角色状态")]
+        [ReadOnly, ShowInInspector] private float m_CurrentSpeed = 0.5f;
+
+        [ReadOnly, ShowInInspector] private bool m_IsInGround = false;
 
         [Header("移动")] 
-        [ReadOnly, ShowInInspector] private float m_CurrentSpeed = 0.5f;
         public float rotationSpeed = 1f;
         public float walkSpeed = 1f;
         public float runeSpeed = 4f;    
         private Vector3 m_MoveDirection;
+
+        [Header("跳跃")] 
+        public float jumpHeight = 2.0f;
+        public float gravity = -9.81f;
+
+        private Vector3 m_VerticalVelocity;
         public float currentSpeed => m_CurrentSpeed;
+        public bool isGrounded => m_IsInGround;
+        
 
         private void Awake()
         {
@@ -34,6 +43,8 @@ namespace Animation3th
         private void Update()
         {
             SetPlayerRotation(Time.deltaTime);
+            SetPlayerGravity(Time.deltaTime);
+            SetPlayerJump(Time.deltaTime);
         }
 
         private void OnAnimatorMove()
@@ -56,7 +67,6 @@ namespace Animation3th
         
             // 当前帧相对于上一帧的位移增量
              Vector3 deltaPosition = m_Animator.deltaPosition;   
-             // 移动 CharacterController
              m_CharacterController.Move(deltaPosition);
         }
 
@@ -65,6 +75,30 @@ namespace Animation3th
             float yRotation = m_Camera.transform.eulerAngles.y;
             Quaternion rotation = Quaternion.Euler(0, yRotation, 0);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * deltaTime);
+        }
+
+        private void SetPlayerGravity(float deltaTime)
+        {
+            m_CharacterController.Move(m_VerticalVelocity * deltaTime);
+            m_IsInGround = m_CharacterController.isGrounded;
+            
+            if (m_IsInGround)
+            {
+                // 当isGrounded为true时，将Y轴方向的速度设为了一个较小的负值，这是为了确保能稳定触碰地面，避免isGrounded误判为false
+                m_VerticalVelocity = new Vector3(0, -2f, 0);
+            }
+            else
+            {
+                m_VerticalVelocity.y += gravity * deltaTime;
+            }
+        }
+
+        private void SetPlayerJump(float deltaTime)
+        {
+            if (m_IsInGround && m_InputController.isJump)
+            {
+                m_VerticalVelocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            }
         }
     }
 }
